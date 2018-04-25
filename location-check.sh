f0a47e722fd770ed8e463b746c4b115408b1f009
T-Rave Flywheel <t-rave@T-Raves-MacBook-Pro.local>
2018-04-25 10:15:00 -0500

    remove location check script

--- location-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## author - https://stackoverflow.com/a/6136861/1718967
-while read LINE; do
-  curl -o /dev/null --silent --head --write-out "%{http_code} $LINE ==> %{url_effective}\n" "$LINE"
-
-done < ./url-test.txt
